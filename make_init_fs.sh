eb78d766b17f0efdeaeb262b98080b0e648e9d8c
muflax <mail@muflax.com>
2011-11-19 11:03:10 +0100

    first commit

---
+++ make_init_fs.sh
@@ -0,0 +1,5 @@
+#!/bin/zsh
+# Copyright muflax <mail@muflax.com>, 2011
+# License: GNU GPL 3 <http://www.gnu.org/copyleft/gpl.html>
+
+find . -print0 | cpio --null -ov --format=newc | gzip -9 > initramfs.cpio.gz
