5652c6737deb9a776150d2972c62602b49227331
muflax <mail@muflax.com>
2013-08-21 15:12:39 +0200

    Revert "don't compress initramfs because we now include it in the kernel"
    This reverts commit 5a793c7f64dfff4c7584a0303303f2a9af54c4c2.

--- make_init_fs.sh
+++ make_init_fs.sh
@@ -2,4 +2,4 @@
 # Copyright muflax <mail@muflax.com>, 2011
 # License: GNU GPL 3 <http://www.gnu.org/copyleft/gpl.html>
 
-find . -not -path "./.git*" -not -path "*.cpio" -print0 | cpio --null -ov --format=newc > initramfs.cpio
+find . -not -path "./.git*" -not -path "*.cpio.gz" -print0 | cpio --null -ov --format=newc | gzip -9 > initramfs.cpio.gz
